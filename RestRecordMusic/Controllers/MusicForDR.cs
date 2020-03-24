3921f480a8d2e646d6dfe98080271c058e1b0db8
Solodanser1809 <futhark1536@gmail.com>
2020-03-24 11:04:36 +0100

    Initial commit

---
+++ RestRecordMusic/Controllers/MusicForDR.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestRecordMusic.Controllers
+{
+    public class MusicForDR
+    {
+        
+            // instance field
+            public string title;
+            public string artist;
+            public int id;
+            public double duration;
+            public int yearOfPublication;
+
+            // propetories
+            public string Title { get; set; }
+            public string Artist { get; set; }
+            public int Id { get; set; }
+            public double Duration { get; set; }
+            public int YearOfPublication { get; set; }
+
+            // constructor
+            public MusicForDR(string title, string artist, int id, double duration, int yearOfPublication)
+            {
+                this.title = title;
+                this.artist = artist;
+                this.id = id;
+                this.duration = duration;
+                this.yearOfPublication = yearOfPublication;
+            }
+            public MusicForDR()
+            {
+
+            }
+
+            public override string ToString()
+            {
+                return Title + Artist.ToString();
+            }
+        
+    }
+
+
+}
